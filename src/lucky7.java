53a0f1088979afc01e8c379c02123eae1e948755
Stuffe <Juuuhooo10@gmail.com>
2025-10-02 10:25:29 +0300

    Random number generator
    Toimii

--- src/lucky7.java
+++ src/lucky7.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
+
 public class lucky7 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Random random = new Random();
+        int randomNumber;
+
+        randomNumber =random.nextInt(10);
+        System.out.println(randomNumber);
     }
 }
