9b947a97390f858362f8a9c3b1d09f1b1854a235
Stuffe <Juuuhooo10@gmail.com>
2025-10-02 13:45:33 +0300

    lucky7
    Hävisit ja Voitit  if ja else

--- src/lucky7.java
+++ src/lucky7.java
@@ -5,9 +5,20 @@ public class lucky7 {
     public static void main(String[] args) throws Exception {
         
         Random random = new Random();
-        int randomNumber;
 
-        randomNumber =random.nextInt(10);
-        System.out.println(randomNumber);
+        int luku1 = random.nextInt(10) + 1;
+        int luku2 = random.nextInt(10) + 1;
+        int luku3 = random.nextInt(10) + 1;
+        System.out.println( luku1 + ", " + luku2 +  ", " +  luku3);   
+
+        if (luku1 == 7 || luku2 == 7 || luku3 == 7){
+            System.out.println("Onnittelut!");
+    
+        } else  {
+        System.out.println("Hävisit!");
+
+    }
+
+
     }
 }
