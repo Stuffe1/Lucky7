56adca58e0f4d3a395037f9a272acd78f1c526a6
Stuffe <Juuuhooo10@gmail.com>
2025-10-02 15:45:15 +0300

    lucky7

--- src/lucky7.java
+++ src/lucky7.java
@@ -5,20 +5,38 @@ public class lucky7 {
     public static void main(String[] args) throws Exception {
         
         Random random = new Random();
+        int i = 5;
+        while (i >0){
+            System.out.println("sinulla on " + i + " Euroa");
+            System.out.println("Maksoit 1 Euron");
+            i --;
+        
+            int luku1 = random.nextInt(10) + 1;
+            int luku2 = random.nextInt(10) + 1;
+            int luku3 = random.nextInt(10) + 1;
 
-        int luku1 = random.nextInt(10) + 1;
-        int luku2 = random.nextInt(10) + 1;
-        int luku3 = random.nextInt(10) + 1;
-        System.out.println( luku1 + ", " + luku2 +  ", " +  luku3);   
+            System.out.println( luku1 + ", " + luku2 +  ", " +  luku3);   
+        
+            int seiska = 0;
+            if (luku1 == 7) seiska++;
+            if (luku2 == 7) seiska++;
+            if (luku3 == 7) seiska++;
 
-        if (luku1 == 7 || luku2 == 7 || luku3 == 7){
-            System.out.println("Onnittelut!");
-    
-        } else  {
-        System.out.println("Hävisit!");
+            if (seiska == 1){
+                i +=3;
+                System.out.println("Voitit 3 Euroa");
+            }else if(seiska == 2){
+                i +=5;
+                System.out.println("Voitit 5 Euroa");
+            }else if(seiska == 3){
+            i +=5;
+            System.out.println("Voitit 10 Euroa");
+            }else{
+                System.out.println("Ei voittoa"); 
+         }
+        }
+        System.out.println("Peli päättyi");{
 
-    }
-
-
-    }
+        }
+}
 }
